0682ed6664777ef98b9afd6cd5487fa603491c8a
@hexlet/cli <support@hexlet.io>
2024-03-02 09:57:22 +0300

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+public class App{
+    public static Map <String, Integer> getWordCount(String sentence) {
+        var resultMap = new HashMap<String, Integer>();
+        if (sentence.isEmpty()) {
+            return resultMap;
+        }
+        var wordsArray = sentence.split(" ");
+        for (var word : wordsArray) {
+            if (resultMap.containsKey(word)) {
+                resultMap.put(word, resultMap.get(word) + 1);
+            } else {
+                resultMap.put(word, 1);
+            }
+        }
+        return resultMap;
+    }
+
+    public static String toString(Map<String, Integer> wordCount) {
+        if (wordCount.isEmpty()) {
+            return "{}";
+        }
+        var entries = wordCount.entrySet();
+        StringBuilder resultStringBuilder = new StringBuilder("{");
+        for(var entry : entries) {
+            resultStringBuilder.append("\n  ").append(entry.getKey()).append(": ").append(entry.getValue());
+        }
+        var resultString = resultStringBuilder.toString();
+        resultString = resultString + "\n}";
+        return resultString;
+    }
+}
+//END
